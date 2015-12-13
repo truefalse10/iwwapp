906962b42070e19ddb195d5e8dcafe878cba232e
serrian <s.dirrigl@yahoo.de>
2015-12-13 19:13:13 +0100

    init express app

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var gutil = require('gutil');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', ['styles', 'watch']);
+
+gulp.task('styles', function() {
+  return gulp.src('public/stylesheets/style.less')
+    .pipe(less())
+    .pipe(autoprefixer())
+    //.pipe(gulpif(production, cssmin()))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('public/stylesheets/**/*.less', ['styles']);
+});
