d1e5886c9ef2b6ace673bd953c82c0d5df9fe284
truefalse10 <truefalse@posteo.de>
2016-02-15 20:30:15 +0100

    making events dynamic

---
+++ routes/events.js
@@ -0,0 +1,139 @@
+var events = {
+  getAllEvents: function() {
+    var result = [
+      {
+        title: "8 Years IWW w/ Truncate",
+        date: "20.02.2016",
+        imgUrl: "images/flyer/2016/truncate-flyer.jpeg",
+        location: "Harry Klein, München",
+        description: "",
+        facebookLink: "https://www.facebook.com/events/929675867117581/",
+        isUpcoming: true,
+        artists: [
+          {
+            name: "TRUNCATE",
+            labels: "50 Weapons, Droid Behavior",
+            city: "LA"
+          },
+          {
+            name: "SEBASTIAN GALVANI",
+            labels: "IWW",
+            city: "Munich"
+          },
+          {
+            name: "FABIAN KRANZ",
+            labels: "IWW",
+            city: "Munich"
+          },
+          {
+            name: "SEPARÉE: MAXÂGE b2b MICHAL ZIETARA",
+            labels: "IWW, Wilde Renate",
+            city: "Munich, Berlin"
+          }
+        ]
+      },
+      {
+        title: "IWW - Explorations in 2016",
+        date: "29.01.2016",
+        imgUrl: "images/flyer/2016/johannes-volk-flyer.jpg",
+        location: "Harry Klein, München",
+        description: "",
+        facebookLink: "https://www.facebook.com/events/1727780684122754/",
+        isUpcoming: false,
+        artists: [
+          {
+            name: "Johannes Volk",
+            labels: "Dolly, Explorations",
+            city: "Gießen"
+          },
+          {
+            name: "SEBASTIAN GALVANI",
+            labels: "IWW",
+            city: "Munich"
+          },
+          {
+            name: "MAXÂGE",
+            labels: "IWW",
+            city: "Munich"
+          },
+          {
+            name: "SEPARÉE: WALTER WOLFF",
+            labels: "Pappen+",
+            city: "Munich"
+          }
+        ]
+      },
+      {
+        title: "IWW - Functional Forward with Delta Funktionen",
+        date: "05.12.2015",
+        imgUrl: "images/flyer/2015/delta-funktionen-flyer_2.jpg",
+        location: "Harry Klein, München",
+        description: "",
+        facebookLink: "https://www.facebook.com/events/1670655039846978/",
+        isUpcoming: false,
+        artists: [
+          {
+            name: "DELTA FUNKTIONEN",
+            labels: "Delsin, Radio Matrix",
+            city: "Holland"
+          },
+          {
+            name: "ANDREA DI ROCCO",
+            labels: "Dirtybird, Soulfooled, IWW",
+            city: "Italy"
+          },
+          {
+            name: "FABIAN KRANZ",
+            labels: "IWW",
+            city: "Munich"
+          },
+          {
+            name: "MAXÂGE",
+            labels: "IWW",
+            city: "Munich"
+          },
+          {
+            name: "SEPARÉE: COSMO THE CAMEL",
+            labels: "IWW",
+            city: "Munich"
+          }
+        ]
+      },
+      {
+        title: "R.I.P 50WEAPONS",
+        date: "24.10.2015",
+        imgUrl: "images/flyer/2015/50weapons_webflyer.jpg",
+        location: "MMA, München",
+        description: "",
+        facebookLink: "https://www.facebook.com/events/416273221903850/",
+        isUpcoming: false,
+        artists: [
+          {
+            name: "SHED",
+            labels: "50 Weapons, Ostgut Ton",
+            city: "Berlin"
+          },
+          {
+            name: "BENJAMIN DAMAGE live",
+            labels: "50 Weapons",
+            city: "Berlin"
+          },
+          {
+            name: "MAXÂGE",
+            labels: "IWW",
+            city: "Munich"
+          },
+          {
+            name: "SEBASTIAN GALVANI",
+            labels: "IWW",
+            city: "Munich"
+          }
+        ]
+      }
+    ];
+    return result;
+  }
+};
+
+
+module.exports = events;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// import events module
+var events = require('./events');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var env = express().get('env');
+  console.log('env:', env);
+  
+  // load events data and inject it into index template
+  var data = events.getAllEvents();
+  res.render('index', { title: 'IWW Music - Events, Label, Party Munich', env: env, data: data});
 });
 
 module.exports = router;
